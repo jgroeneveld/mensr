54377d20283ac8133e9fea56b6bbf4f9bdc9f27e
Jaap Groeneveld <jaapgr@gmail.com>
2011-03-13 21:07:36 +0100

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ MensaBewerter::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.middleware.use Rack::SslEnforcer
 end
