ecd3252f4d1e03ffaf4ae92b7083a69981ba0cec
Jaap Groeneveld <jaapgr@gmail.com>
2011-03-17 17:12:13 +0100

    feedback

---
+++ public/javascripts/feedback.js
@@ -0,0 +1,18 @@
+var _ues = {
+host:'mensr.userecho.com',
+forum:'4830',
+lang:'de',
+tab_corner_radius:10,
+tab_font_size:14,
+tab_image_hash:'RmVlZGJhY2s%3D',
+tab_alignment:'right',
+tab_text_color:'#020872',
+tab_bg_color:'#B2D3FB',
+tab_hover_color:'#89F462'
+};
+
+(function() {
+    var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+    _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+  })();
